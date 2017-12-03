d7d6e153064bef5969fccdab1726fdb21e3cca66
Thom Page <thom.page.000@gmail.com>
2017-12-03 16:19:47 -0500

    adds boilerplate code

---
+++ server.js
@@ -0,0 +1,21 @@
+const express  = require('express');
+const mongoose = require('mongoose');
+const app      = express();
+
+// process.env.PORT
+const PORT = 3000;
+
+// process.env.MONGODB_URI
+const mongoURI = 'mongodb://localhost/student-test-deploy';
+
+mongoose.connect(mongoURI, { useMongoClient: true });
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.on('error', (err) => console.log('Mongo error: ', err));
+db.on('connected', () => console.log('Mongo connected: ', mongoURI));
+
+// routes
+app.get('/', (req, res) => res.send('Welcome'));
+
+app.listen(PORT, () => console.log('App running on port: ', PORT));
